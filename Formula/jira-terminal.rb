1a8fcd6d2fd03119b1fe444e0eb0ab1b3e350bba
Amrit Ghimire <mail@amritghimire.com>
2022-09-15 15:56:07 +0545

    Update jira-terminal.rb

--- Formula/jira-terminal.rb
+++ Formula/jira-terminal.rb
@@ -4,9 +4,9 @@
 class JiraTerminal < Formula
   desc "The application that can be used for personal usage to manage jira from terminal. " 
   homepage "https://github.com/amritghimire/jira-terminal"
-  url "https://github.com/amritghimire/jira-terminal/archive/refs/tags/v1.6.0.tar.gz"
-  sha256 "0f11a14462240ee40fdc477df6a60255c7dc6c8869ad93edb0ae55dda588669e"
-  version "1.6.0"
+  url "https://github.com/amritghimire/jira-terminal/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "74b626520739f21c136ad1036d71f3f00940405c85c8488e0eabd512464efc2e"
+  version "2.0.0"
   depends_on "rust" => :build
 
   def install
